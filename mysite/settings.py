ce2e272f4fffb85fc070ef29dcec576b8081caf0
BigBlueNice419 <rlewis2@andover.edu>
2019-04-15 18:31:16 -0400

    commented out

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Configure Django App for Heroku.
 import django_heroku
